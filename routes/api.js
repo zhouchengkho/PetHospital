34743e357cac539a27654f328ab8236a051f155c
zhouchengkho <741713939@qq.com>
2017-03-02 12:20:42 +0800

    learn

--- routes/api.js
+++ routes/api.js
@@ -7,11 +7,11 @@ var uuid = require('uuid');
 /** API **/
 
 /**
- * @api {post} /user/login User Login
+ * @api {post} /api/login User Login
  * @apiName User Login
- * @apiGroup User
+ * @apiGroup API
  *
- * @apiParam {String} username
+ * @apiParam {String} operation   'add' | ''
  * @apiParam {String} password
  *
  * @apiSuccessExample Success-Response
@@ -55,5 +55,53 @@ router.get('/test', function(req, res) {
   })
 })
 
-
+/**
+ * @api {post} /api/user User Operation
+ * @apiName User Operation
+ * @apiGroup User
+ *
+ * @apiRequestExample
+ * {
+ *  "operation": "add",
+ *  "name": "wangpiaoliang",
+ *  "password": "123",
+ *  "role": "intern"
+ * }
+ *
+ *
+ * {
+ *  "operation": "delete",
+ *  "name": "wangpiaoliang",
+ *  "token" : "wangpiaoliang's working token"
+ * }
+ *
+ *
+ * {
+ *  "operation": "update",
+ *  "token": "wangxiaodui's working token"
+ *  "name": "wangpiaoliang",
+ *  "new-name": "wangxiaodui",
+ *  "new-password": "new pass",
+ *  "new-role": "admin" // this should be optional
+ * }
+ *
+ * @apiSuccessExample Success-Response
+ * HTTP/1.1 200 ok
+ * {
+ *  "data":
+ *    {
+ *      "message": "Success"
+ *    },
+ *  "status": 200
+ * }
+ * @apiError 400 Bad User Input
+ * @apiErrorExample 400 Error: Login Failed
+ *  HTTP/1.1 400 Bad Input
+ *  {
+ *    "message":  "Failed Reason",
+ *    "status" : 400
+ *  }
+ *
+ *
+ */
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -24,12 +24,26 @@ router.get('/', function(req, res) {
 });
 
 router.get('/login', function(req, res) {
+
     res.render('login', {
       title: 'login' ,
       script: '<script type="text/javascript" src="/js/login.js"></script>'
     });
 });
 
+router.get('/learn', function(req, res) {
+  res.render('learn')
+})
 
+router.get('/learn/reception', function(req, res) {
+  res.render('reception')
+})
 
+router.get('/learn/vet', function(req, res) {
+  res.render('vet')
+})
+
+router.get('/learn/assistant', function(req, res) {
+  res.render('assistant')
+})
 module.exports = router;
