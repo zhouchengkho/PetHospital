71fa71bf5ed0584fbe4c004e219bd312f0710848
Spikerman <spikechenhao@163.com>
2017-03-07 13:07:45 +0800

    bug fix

--- routes/index.js
+++ routes/index.js
@@ -3,47 +3,63 @@ var router = express.Router();
 
 /* GET home page. */
 
-router.get('/', function(req, res) {
-
-  var data = {
-    title: 'Pet Hospital',
-    data: [
-      {
-        name: 'Cat',
-        treatment: 'Needle'
-      }, {
-        name: 'Dog',
-        treatment: 'Medicine'
-      }, {
-        name: 'Pig',
-        treatment: 'Kill'
-      }
-    ]
-  }
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res) {
+    var data = {
+        title: 'Pet Hospital',
+        data: [
+            {
+                name: 'Cat',
+                treatment: 'Needle'
+            }, {
+                name: 'Dog',
+                treatment: 'Medicine'
+            }, {
+                name: 'Pig',
+                treatment: 'Kill'
+            }
+        ]
+    }
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/login', function(req, res) {
+res.render('login', {
+    title: 'login',
+    script: '<script type="text/javascript" src="/js/login.js"></script>'
 
-    res.render('login', {
-      title: 'login' ,
-      script: '<script type="text/javascript" src="/js/login.js"></script>'
-    });
 });
 
-router.get('/learn', function(req, res) {
-  res.render('learn')
+router.get('/case', function (req, res) {
+    var data = {
+        title: 'Pet Hospital',
+        data: [
+            {
+                name: 'Cat',
+                treatment: 'Needle'
+            }, {
+                name: 'Dog',
+                treatment: 'Medicine'
+            }, {
+                name: 'Pig',
+                treatment: 'Kill'
+            }
+        ]
+    }
+    res.render('case', {test: data});
+});
+router.get('/learn', function (req, res) {
+    res.render('learn')
 })
 
-router.get('/learn/reception', function(req, res) {
-  res.render('reception')
+router.get('/learn/reception', function (req, res) {
+    res.render('reception')
 })
 
-router.get('/learn/vet', function(req, res) {
-  res.render('vet')
+router.get('/learn/vet', function (req, res) {
+    res.render('vet')
 })
 
-router.get('/learn/assistant', function(req, res) {
-  res.render('assistant')
+router.get('/learn/assistant', function (req, res) {
+    res.render('assistant')
 })
+
 module.exports = router;
